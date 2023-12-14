abb0721076589ae71ef214d37c0b728083e2e29e
Carlos Saltrón <csaltron@MacBook-Pro-14-de-Carlos.local>
2023-12-14 09:20:47 -0300

    MapStruct implementation

--- src/main/java/com/inditex/ecommerce/rest/controller/PricesController.java
+++ src/main/java/com/inditex/ecommerce/rest/controller/PricesController.java
@@ -2,9 +2,8 @@ package com.inditex.ecommerce.rest.controller;
 
 import com.inditex.ecommerce.domain.entities.Price;
 import com.inditex.ecommerce.domain.usecase.PricesPort;
-import com.inditex.ecommerce.rest.dto.BrandDTO;
 import com.inditex.ecommerce.rest.dto.PriceDTO;
-import com.inditex.ecommerce.rest.dto.ProductDTO;
+import com.inditex.ecommerce.rest.mapper.PriceMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
@@ -23,8 +22,11 @@ public class PricesController {
     private final PricesPort pricesPort;
 
 
-    public PricesController(final PricesPort pricesPort){
+    private final PriceMapper priceMapper;
+
+    public PricesController(final PricesPort pricesPort, PriceMapper priceMapper){
         this.pricesPort = pricesPort;
+        this.priceMapper = priceMapper;
     }
 
     @GetMapping(path = "/prices")
@@ -35,24 +37,7 @@ public class PricesController {
             ) {
         log.info("Test");
         Price price = pricesPort.search(brandId, productId, applicationDate);
-        PriceDTO priceDTO = PriceDTO.builder()
-                .price(price.getAmount())
-                .brand(BrandDTO.builder()
-                        .id(price.getBrand().getId())
-                        .name(price.getProduct().getName())
-                        .build())
-                .product(ProductDTO.builder()
-                        .id(price.getProduct().getId())
-                        .name(price.getProduct().getName())
-                        .build())
-                .curr(price.getCurr())
-                .priority(price.getPriority())
-                .priceList(price.getPriceList())
-                .startDate(price.getStartDate().toLocalDate())
-                .endDate(price.getEndDate().toLocalDate())
-                .build();
-
-
+        PriceDTO priceDTO = priceMapper.toPriceDto(price);
 
         return ResponseEntity.ok().body(priceDTO);
 
--- src/main/java/com/inditex/ecommerce/rest/mapper/PriceMapper.java
+++ src/main/java/com/inditex/ecommerce/rest/mapper/PriceMapper.java
@@ -3,17 +3,28 @@ package com.inditex.ecommerce.rest.mapper;
 import com.inditex.ecommerce.domain.entities.Price;
 import com.inditex.ecommerce.rest.dto.BrandDTO;
 import com.inditex.ecommerce.rest.dto.PriceDTO;
+import com.inditex.ecommerce.rest.dto.ProductDTO;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring",injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public interface PriceMapper {
     default PriceDTO toPriceDto(Price price){
-        //TODO Completar campos
-        return PriceDTO
-                .builder()
+                return PriceDTO.builder()
                 .price(price.getAmount())
-                .brand(BrandDTO.builder().id(price.getBrand().getId()).build())
+                .brand(BrandDTO.builder()
+                        .id(price.getBrand().getId())
+                        .name(price.getProduct().getName())
+                        .build())
+                .product(ProductDTO.builder()
+                        .id(price.getProduct().getId())
+                        .name(price.getProduct().getName())
+                        .build())
+                .curr(price.getCurr())
+                .priority(price.getPriority())
+                .priceList(price.getPriceList())
+                .startDate(price.getStartDate().toLocalDate())
+                .endDate(price.getEndDate().toLocalDate())
                 .build();
     }
 }
